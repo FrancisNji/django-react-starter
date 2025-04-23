4ac865bf517068c2d76774231296b10423e8d7a2
Frankie0609 <33159972+Frankie0609@users.noreply.github.com>
2025-04-23 08:41:07 -0400

    added10

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,12 @@ echo "---- [ApplicationStart] Starting Django and React Dev Servers ----"
 
 cd /home/ec2-user/django-react-starter/backend
 
+
+if [ ! -f "venv/bin/activate" ]; then
+  echo "Virtual environment not found! Exiting..."
+  exit 1
+fi
+
 # Activate virtual environment
 source venv/bin/activate
 
@@ -11,8 +17,8 @@ source venv/bin/activate
 nohup python manage.py runserver 0.0.0.0:8000 &
 
 # (Optional) Start React dev server too
-cd ../frontend
-nohup npm run dev &
+# cd ../frontend
+# nohup npm run dev &
 
 echo "---- [ApplicationStart] Servers started ----"
 
