8bb9c6d2a241e7aac2d7cd8296dc5d81af7ebdb5
Frankie0609 <33159972+Frankie0609@users.noreply.github.com>
2025-04-23 08:37:31 -0400

    added9

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -e
+
 echo "---- [ApplicationStart] Starting Django and React Dev Servers ----"
 
 cd /home/ec2-user/django-react-starter/backend
+
+# Activate virtual environment
 source venv/bin/activate
+
+# Run Django dev server in background
 nohup python manage.py runserver 0.0.0.0:8000 &
 
+# (Optional) Start React dev server too
 cd ../frontend
 nohup npm run dev &
+
+echo "---- [ApplicationStart] Servers started ----"
+
