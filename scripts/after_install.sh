fb41623c8edc34ab9e3f8de927c3980e195b90c4
Frankie0609 <33159972+Frankie0609@users.noreply.github.com>
2025-04-23 12:15:24 -0400

    update10

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -11,9 +11,9 @@ python manage.py migrate
 echo "==== Installing frontend dependencies ===="
 cd /home/ec2-user/django-react-starter/frontend
 npm install --legacy-peer-deps
-npm run build
+npm run dev
 
-# Move built frontend to Django static
-echo "==== Moving React dist to Django ===="
-rm -rf /home/ec2-user/django-react-starter/backend/frontend/dist
-cp -r /home/ec2-user/django-react-starter/frontend/dist /home/ec2-user/django-react-starter/backend/frontend/dist
+# # Move built frontend to Django static
+# echo "==== Moving React dist to Django ===="
+# rm -rf /home/ec2-user/django-react-starter/backend/frontend/dist
+# cp -r /home/ec2-user/django-react-starter/frontend/dist /home/ec2-user/django-react-starter/backend/frontend/dist
