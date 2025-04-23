ff268c70c0d2d83ece9d6519dbe435b58bfb9c9a
Frankie0609 <33159972+Frankie0609@users.noreply.github.com>
2025-04-23 14:17:08 -0400

    update20

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,12 +2,18 @@
 
 echo "==== [AfterInstall] Running Django backend setup ===="
 
-cd /home/ec2-user/django-react-starter/backend
+cd /home/ec2-user/django-react-starter
 
 # Activate virtual environment
+
+python3 -m venv venv
 source venv/bin/activate
 
+cd /home/ec2-user/django-react-starter/backend
+
 # Run database migrations
+
+python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
@@ -18,8 +24,16 @@ echo "==== [AfterInstall] Building React frontend ===="
 cd /home/ec2-user/django-react-starter/frontend
 
 # Ensure Node.js is installed and available
-npm install
+npm install --legacy-peer-deps
 npm run build
+
+cd /home/ec2-user/django-react-starter/backend
+
+python manage.py runserver
+
+cd /home/ec2-user/django-react-starter/frontend
+
+npm run dev
 
 echo "==== [AfterInstall] Deployment steps completed successfully ===="
 
